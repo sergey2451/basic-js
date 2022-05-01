db8c7f04275bc0bc3df15e3ab85f750a7f9eb41b
sergey2451 <kragel.s@mail.ru>
2022-05-01 22:22:57 +0300

    feat: complete task what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-
+function getSeason(date) {
+	if (date === undefined) {
+		return 'Unable to determine the time of year!';
+	} else {
+		const arr = Object.keys(date);
+		if (arr.length > 0) {
+			throw new Error('Invalid date!');
+		}
+	}
+	if ((-Date.parse(date) + Date.parse(date)) === 0) {
+		const month = date.getMonth();
+		if (month === 11 || month <= 1) {
+			return 'winter';
+		} else if (month >= 2 && month <= 4) {
+			return 'spring';
+		} else if (month >= 5 && month <= 7) {
+			return 'summer';
+		} else if (month >= 8 && month <= 10) {
+			return 'autumn';
+		} 
+	} else {
+		throw new Error('Invalid date!');
+	}
 }
 
 module.exports = {
